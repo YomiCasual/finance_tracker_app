7851ba3b219bf846a8a04c0a45e50d4d639f9d4f
Abiodun Onisade <qabiodun@quidax.com>
2023-01-01 15:04:24 +0100

    feat: add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'my_portfolio', to "users#my_portfolio"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index' 
+
+  resources :stocks
 
 end
