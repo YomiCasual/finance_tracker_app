160f295b2c972b73735d3a48b96e89b376ec75f8
Abiodun Onisade <qabiodun@quidax.com>
2023-01-07 01:42:27 +0100

    feat: allow first aname and last name in user model devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
 
 
 	def handle_error_redirect(error_text: "An error occured", redirect_url: root_path, is_redirect: true)
@@ -19,6 +21,12 @@ class ApplicationController < ActionController::Base
 			format.turbo_stream
 			return
 		end
+	end
+
+	protected
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+	  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
 	end
 
 
--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -13,44 +13,48 @@ class UserStocksController < ApplicationController
 			end
 		end
 
+		existing_stock = UserStock.find_by(user: current_user, stock: stock )  
+
+		
+		if existing_stock
+			p "existing_stock #{existing_stock}"
+			flash.now[:alert] = "Stock already tracked"
+			respond_with_stream
+			return
+		end
+
 		@new_user_stock = UserStock.create(user: current_user, stock_id: stock.id )
+
+		@stock = stock
 
 		if @new_user_stock
 			respond_to do |format|
-				flash.now[:notice] = "Stock created successfully"
+				flash.now[:notice] = "Stock added successfully"
 				format.html
-				format.turbo_stream {
-					render turbo_stream: turbo_stream.append('stock_list', partial: 'users/shared/stock_list', 
-					locals: { stock: stock}
-					)
-				}
+				format.turbo_stream
 			end
-			# respond_with_stream
 		else
 			flash.now[:alert] = "Could not add stock to portfolio"
 			respond_with_stream
+			return
 		end
 
 	end
 
 	def destroy
 
-
 		@stock = UserStock.find_by(stock_id: delete_stock_params, user: current_user);
+		@params =delete_stock_params
 		if !@stock
 			flash.now[:alert] = "Cannot find stock"
 			respond_with_stream
 		end
 
-		p "stock #{@stock}"
-
-
 		if @stock.destroy
 			respond_to do |format|
-				flash.now[:notice] = "Stock removed successfully"
-				format.turbo_stream {
-					render turbo_stream: turbo_stream.remove("stock_list_#{delete_stock_params}", ) }
-				format.html { redirect_to my_portfolio_path, notice: "Stock was successfully removed" }
+				flash.now[:alert] = "Stock removed successfully"
+				format.html 
+				format.turbo_stream 
 			end
 		else
 			flash.now[:alert] = "Could not remove stock"
